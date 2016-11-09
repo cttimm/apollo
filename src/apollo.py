7491f04fd12e49d0cd4578e70dcf747381817598
cttimm <charlesttimmerman@gmail.com>
2016-11-09 10:34:41 -0500

    Testing, proof of concept

--- src/apollo.py
+++ src/apollo.py
@@ -24,7 +24,7 @@ def activationFunction(x):
     return math.tanh(x)
 
 #Dx of Sigmoid activation function
-def dactivationFunction(y):
+def dxactivationFunction(y):
     return 1.0 - y**2
 
 class NN:
@@ -34,9 +34,9 @@ class NN:
         self.n_output = n_output
 
         # Activations for nodes
-        self.a_input = [1.0]*self.n_input
-        self.a_hidden = [1.0]*self.n_hidden
-        self.a_output = [1.0]*self.n_output
+        self.a_input = [1.0] * self.n_input
+        self.a_hidden = [1.0] * self.n_hidden
+        self.a_output = [1.0] * self.n_output
 
         # Weights
         self.w_input = initMatrix(self.n_input, self.n_hidden)
@@ -61,21 +61,21 @@ class NN:
         
         # Input activations
         for i in range(self.n_input-1):
-            self.a_input = inputs[i]
+            self.a_input[i] = inputs[i]
 
         # Hidden activations
         for j in range(self.n_hidden):
             sum = 0.0
             for i in range(self.n_input):
                 sum = sum + self.a_input[i] * self.w_input[i][j]
-            self.a_hidden[j] = sigmoid(sum)
+            self.a_hidden[j] = activationFunction(sum)
                 
          # Output activations
-         for k in range(self.n_output):
+        for k in range(self.n_output):
             sum = 0.0
             for j in range(self.n_hidden):
-                sum = sum + self.a_hidden[j] * self.w_output[i][j]
-            self.a_output[k] = sigmoid(sum) 
+                sum = sum + self.a_hidden[j] * self.w_output[j][k]
+            self.a_output[k] = activationFunction(sum) 
         return self.a_output[:]
 
     # Calculate error (output, hidden), update input and output weights
@@ -83,20 +83,43 @@ class NN:
     def backProp(self, targets, N, M):
         
         # Calculate Output Error
+        output_deltas = [0.0] * self.n_output
+        for k in range(self.n_output):
+            error = targets[k] - self.a_output[k]
+            output_deltas[k] = dxactivationFunction(self.a_output[k]) * error
 
         # Calculate Hidden Error
+        hidden_deltas = [0.0] * self.n_hidden
+        for j in range(self.n_hidden):
+            error = 0.0
+            for k in range(self.n_output):
+                error = error + output_deltas[k] * self.w_output[j][k]
+            hidden_deltas[j] = dxactivationFunction(self.a_hidden[j]) * error
 
         # Update Output Weights
+        for j in range(self.n_hidden):
+            for k in range(self.n_output):
+                delta = output_deltas[k] * self.a_hidden[j]
+                self.w_output[j][k] = self.w_output[j][k] + (N * delta) + (M * self.c_output[j][k])
+                self.c_input[j][k] = delta
 
         # Update Input Weights
+        for i in range(self.n_input):
+            for j in range(self.n_hidden):
+                delta = hidden_deltas[j] * self.a_input[i]
+                self.w_input[i][j] = self.w_input[i][j] + (N * delta) + (M * self.c_input[i][j])
+                self.c_input[i][j] = delta
 
         # Calculate Error
-
+        error = 0.0
+        for k in range(len(targets)):
+            error = error + 0.5*(targets[k] - self.a_output[k])**2
         return error
     
     # Test the class with a pattern
-    def test(self):
-        pass
+    def test(self, patterns):
+        for p in patterns:
+            print(p[0], ' --> ', self.update(p[0]))
 
     # Print the weights
     def weights(self):
@@ -104,5 +127,13 @@ class NN:
 
     # Train the NN, N = learning rate, M = momentum factor
     def train(self, patterns, iterations=1000, N=0.5, M=0.1):
-        pass
+        for i in range(iterations):
+            error = 0.0
+            for p in patterns:
+                inputs = p[0]
+                targets = p[1]
+                self.update(inputs)
+                error = error + self.backProp(targets, N, M)
+            if i % 100 == 0:
+                print("Error %-.5f" % error)
     
--- src/test.py
+++ src/test.py
@@ -1,5 +1,19 @@
 import apollo
 
-test = apollo.NN(2, 2, 2)
+test = apollo.NN(3, 2, 1)
 
-print(test.w_output)
+# Proof of concept
+# [gpm, xpm, kills],[1 = win, 0 = loss]
+pattern = [
+    [[.7,.700,.2],[1]],
+    [[.300,.300,.01],[0]],
+    [[.540,.540,.05],[1]]
+]
+
+test.train(pattern)
+
+#.96 certainty of win
+test.test([[[.500,.500,.11]]])
+
+#.08 certainty of win
+test.test([[[.300,.300,.05]]])
